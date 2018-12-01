42d096f9915c4036a6094d520b8b031df56073e2
Lam Le <15520405@gm.uit.edu.vn>
2018-12-01 12:09:55 +0700

    Set up Azure Service App and made swagger public in release mode

--- ParkEco.CoreAPI/ParkEco.CoreAPI/Startup.cs
+++ ParkEco.CoreAPI/ParkEco.CoreAPI/Startup.cs
@@ -59,21 +59,21 @@ namespace ParkEco.CoreAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
-                // Enable middleware to serve generated Swagger as a JSON endpoint.
-                app.UseSwagger();
-
-                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
-                // specifying the Swagger JSON endpoint.
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                });
             }
             else
             {
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
