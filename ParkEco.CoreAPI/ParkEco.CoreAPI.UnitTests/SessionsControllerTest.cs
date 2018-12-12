4d82398ed6eacf1c394039b5abdc5915ee52c800
Lam Le <15520405@gm.uit.edu.vn>
2018-12-12 16:29:25 +0700

    Updated new unit tests for sessions APIs

--- ParkEco.CoreAPI/ParkEco.CoreAPI.UnitTests/SessionsControllerTest.cs
+++ ParkEco.CoreAPI/ParkEco.CoreAPI.UnitTests/SessionsControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
 using ParkEco.CoreAPI.Controllers;
 using ParkEco.CoreAPI.Services.Interfaces;
 using System;
@@ -38,6 +39,65 @@ namespace ParkEco.CoreAPI.UnitTests
                     It.Is<Guid>(value => value == expectedGuid)
                 ),
                 Times.Once);
+        }
+
+        [Fact]
+        public void CreateNewSession_ShouldReturn500_WhenCreateMethodThrows()
+        {
+            var mockSessionService = new Mock<ISessionService>();
+            Guid expectedGuid = Guid.NewGuid();
+            DateTime expectedFrom = new DateTime();
+            DateTime expectedTo = new DateTime();
+            mockSessionService.Setup(service =>
+                service.Create(It.Is<DateTime>(value => value == expectedFrom),
+                    It.Is<DateTime>(value => value == expectedTo),
+                    It.Is<Guid>(value => value == expectedGuid)))
+                .Throws(new Exception());
+
+            var controller = new SessionsController(mockSessionService.Object);
+            var result = controller.CreateNewSession(new Controllers.Models.CreateNewSessionCommand()
+            {
+                From = expectedFrom,
+                To = expectedTo,
+                ParkingLotId = expectedGuid
+            }) as Microsoft.AspNetCore.Mvc.StatusCodeResult;
+
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
+        }
+
+        [Fact]
+        public void DeleteSession_ShouldCallDeleteMethodInService()
+        {
+            var mockSessionService = new Mock<ISessionService>();
+            Guid expectedGuid = Guid.NewGuid();
+            mockSessionService.Setup(service =>
+                service.Delete(It.Is<Guid>(value => value == expectedGuid)))
+                .Verifiable();
+
+            var controller = new SessionsController(mockSessionService.Object);
+            var result = controller.DeleteSession(expectedGuid);
+
+            mockSessionService.Verify(service =>
+                service.Delete(
+                    It.Is<Guid>(value => value == expectedGuid)
+                ),
+                Times.Once);
+        }
+
+        [Fact]
+        public void DeleteSession_ShouldReturn500_WhenDeleteMethodThrows()
+        {
+            var mockSessionService = new Mock<ISessionService>();
+            Guid expectedGuid = Guid.NewGuid();
+            mockSessionService.Setup(service =>
+                service.Delete(It.Is<Guid>(value => value == expectedGuid)))
+                .Throws(new Exception());
+
+            var controller = new SessionsController(mockSessionService.Object);
+            var result = controller.DeleteSession(expectedGuid) 
+                as Microsoft.AspNetCore.Mvc.StatusCodeResult;
+
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
         }
     }
 }
